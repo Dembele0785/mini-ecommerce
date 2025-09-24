0735925e9790ecb8597cdd7e9263ff58b7020d24
dembele0785 <dembelebtssio@gmail.com>
2025-09-24 21:31:55 +0200

    Ajout des routes et des templates pour products et contact

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template
 
-main = Blueprint("main", __name__)
+main = Blueprint('main', __name__)
 
 @main.route("/")
-def index():
+def home():
     return render_template("index.html")
+
+@main.route("/products")
+def products():
+    return render_template("products.html")
+
+@main.route("/contact")
+def contact():
+    return render_template("contact.html")
